4ee66a1dd747ab03a1a23a458d427985499174bc
ldabudiab <ldabudiab@gmail.com>
2021-11-18 14:48:43 -0600

    done with phase 4

---
+++ Robe.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace Quest 
+{
+    public class Robe 
+    {
+
+       public  List<string> colors = new List<string> {
+            "red", 
+        };
+        
+        public int Length = 5;
+    }
+}
